2ec244f07fe5606fdb98cc0c2047c6ad59454ff8
Josué Montero <josuedmh1104@gmail.com>
2026-02-18 21:58:41 -0600

    feat: initial spring boot project setup

---
+++ Practica1/src/main/java/com/practica/PracticaProgramadaApplication.java
@@ -0,0 +1,13 @@
+package com.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaProgramadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaProgramadaApplication.class, args);
+	}
+
+}
