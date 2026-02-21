d7f1bd96f201f3a3c406d3503b29ae6c1c5f5fc5
Isaac Hernandez <isaac.f.hernandez@outlook.com>
2026-02-21 12:44:25 -0600

    Created service classes for CRUD Interactions

--- Practica1_Web_Empresarial/src/main/java/com/practica/service/CursoService.java
+++ Practica1_Web_Empresarial/src/main/java/com/practica/service/CursoService.java
@@ -1,4 +1,44 @@
 package com.practica.service;
 
+
+import com.practica.domain.Curso;
+import com.practica.repository.CursoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CursoService {
+
+    private final CursoRepository cursoRepository;
+
+    public CursoService(CursoRepository cursoRepository) {
+        this.cursoRepository = cursoRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Curso> getCursos(){
+        return cursoRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Curso> getCurso(Integer idCurso){
+        return cursoRepository.findById(idCurso);
+    }
+
+    @Transactional
+    public Curso save(Curso curso){
+        return cursoRepository.save(curso);
+    }
+
+    @Transactional
+    public void delete(Integer idCurso){
+        if(cursoRepository.existsById(idCurso)){
+            cursoRepository.deleteById(idCurso);
+        } else {
+            throw new IllegalArgumentException("No existe el " + idCurso);
+        }
+    }
 }
--- Practica1_Web_Empresarial/src/main/java/com/practica/service/EstudianteService.java
+++ Practica1_Web_Empresarial/src/main/java/com/practica/service/EstudianteService.java
@@ -1,4 +1,49 @@
 package com.practica.service;
 
+
+import com.practica.domain.Estudiante;
+import com.practica.repository.EstudianteRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EstudianteService {
+
+    private final EstudianteRepository estudianteRepository;
+
+    public EstudianteService(EstudianteRepository estudianteRepository) {
+        this.estudianteRepository = estudianteRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Estudiante> getEstudiantes(){
+        return estudianteRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Estudiante> getEstudiantesbyCurso(Integer idCurso){
+        return estudianteRepository.findByCursoIdCurso(idCurso);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Estudiante> getEstudiante(Integer idEstudiante){
+        return estudianteRepository.findById(idEstudiante);
+    }
+
+    @Transactional
+    public Estudiante save(Estudiante estudiante){
+        return estudianteRepository.save(estudiante);
+    }
+
+    @Transactional
+    public void delete(Integer idEstudiante){
+        if(estudianteRepository.existsById(idEstudiante)){
+            estudianteRepository.deleteById(idEstudiante);
+        } else {
+            throw new IllegalArgumentException("No existe el " + idEstudiante);
+        }
+    }
 }
