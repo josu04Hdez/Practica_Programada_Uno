14074ed170ca4b6c7ef75809f8fbb854fa1420e1
Isaac Hernandez <isaac.f.hernandez@outlook.com>
2026-02-21 12:10:02 -0600

    Generados los elementos de domain para entity, puede dar errores con lombok data, si no lo modifico a getters y setters directos

--- Practica1_Web_Empresarial/src/main/java/com/practica/domain/Curso.java
+++ Practica1_Web_Empresarial/src/main/java/com/practica/domain/Curso.java
@@ -1,4 +1,47 @@
 package com.practica.domain;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
+import java.util.List;
+
+@Entity
+@Table(name = "curso")
+@Data
 public class Curso {
+
+    //Definicion de atributos de clase clasicos de JPA, de jo listo la auto incrementacion del id.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_curso")
+    private Integer idCurso;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nombre;
+
+    @NotBlank
+    @Size(max = 100)
+    private String descripcion;
+
+    @NotNull
+    @Min(0)
+    private Integer creditos;
+
+    @NotNull
+    private Boolean estado;
+
+    @Size(max = 255)
+    private String imagen;
+
+    @OneToMany(mappedBy = "curso")
+    private List<Estudiante> estudiantes;
+
+
+
+
 }
--- Practica1_Web_Empresarial/src/main/java/com/practica/domain/Estudiante.java
+++ Practica1_Web_Empresarial/src/main/java/com/practica/domain/Estudiante.java
@@ -1,5 +1,12 @@
 package com.practica.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -8,6 +15,26 @@ import jakarta.persistence.Table;
 @Data
 @Table(name = "estudiante")
 public class Estudiante {
+
+    //Definicion de atributos de clase clasicos de JPA, de jo listo la auto incrementacion del id.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estudiante")
+    private Integer idEstudiante;
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    private String correo;
+
+    @NotNull
+    @Min(1)
+    private Integer edad;
+
+    @ManyToOne
+    @JoinColumn(name = "id_curso", nullable = false)
+    private Curso curso;
+
 
 
 
